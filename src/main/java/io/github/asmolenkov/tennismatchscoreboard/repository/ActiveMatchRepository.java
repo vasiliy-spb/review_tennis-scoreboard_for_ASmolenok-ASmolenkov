c2e5c556e454b8d5b14fc5feb110874fcbe2dc4b
ASmolenkov <a.smolenkov@list.ru>
2026-07-01 18:36:00 +0300

    refactor(ActiveMatchRepository) - "Магические" строки перенесены в константы. - добавлено логирование сохраненного матча. - Удален неиспользуемый метод update - добавлена проверка на null параметров метода save

--- src/main/java/io/github/asmolenkov/tennismatchscoreboard/repository/ActiveMatchRepository.java
+++ src/main/java/io/github/asmolenkov/tennismatchscoreboard/repository/ActiveMatchRepository.java
@@ -1,24 +1,37 @@
 package io.github.asmolenkov.tennismatchscoreboard.repository;
 
+import io.github.asmolenkov.tennismatchscoreboard.exception.SaveActiveMatchException;
+import io.github.asmolenkov.tennismatchscoreboard.exception.SaveMatchException;
 import io.github.asmolenkov.tennismatchscoreboard.model.CurrentMatch;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-
+@Slf4j
 public class ActiveMatchRepository {
+
+    private static final String LOG_MATCH_SAVE_TEMPLATE = "Матч {} - {} сохранен";
+    private static final String CURRENT_MATCH_NULL = "Current match must not be null";
+    private static final String MATCH_UUID_NULL = "Match UUID must not be null";
+
     private final Map<UUID, CurrentMatch> activeMatches = new ConcurrentHashMap<>();
 
     public void save(CurrentMatch currentMatch){
+        if (currentMatch == null) {
+            throw new SaveActiveMatchException(CURRENT_MATCH_NULL);
+        }
+        if (currentMatch.getUuid() == null) {
+            throw new SaveActiveMatchException(MATCH_UUID_NULL);
+        }
         activeMatches.put(currentMatch.getUuid(),currentMatch);
+        log.info(LOG_MATCH_SAVE_TEMPLATE,currentMatch.getPlayerOne().name(), currentMatch.getPlayerSecond().name());
     }
 
     public Optional<CurrentMatch> find(UUID uuid){
         return Optional.ofNullable(activeMatches.get(uuid));
     }
 
-    public void update(CurrentMatch currentMatch) {
-        activeMatches.replace(currentMatch.getUuid(),currentMatch);
-    }
+
 }
