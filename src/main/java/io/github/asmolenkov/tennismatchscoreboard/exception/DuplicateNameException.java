2dab690e749a7a363ecd0401e99b01149d5d5a71
ASmolenkov <a.smolenkov@list.ru>
2026-07-09 20:18:30 +0300

    debag(DuplicateNameException): - Исправлено отсутствие сообщения когда имена одинаковые

--- src/main/java/io/github/asmolenkov/tennismatchscoreboard/exception/DuplicateNameException.java
+++ src/main/java/io/github/asmolenkov/tennismatchscoreboard/exception/DuplicateNameException.java
@@ -1,6 +1,7 @@
 package io.github.asmolenkov.tennismatchscoreboard.exception;
 
 public class DuplicateNameException extends RuntimeException{
-    public DuplicateNameException(String string) {
+    public DuplicateNameException(String message) {
+        super(message);
     }
 }
